bdb9df3d4c2ddf869f1db943d8093017e18975a2
justastranger <justastrangerlica@gmail.com>
2015-05-11 22:15:46 -0500

    Initial Commit

---
+++ Twitchy/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿using System.Windows.Forms;
+
+namespace Twitchy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(13, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Launch Stream";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(150, 204);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Refresh List";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(13, 179);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(116, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Close After Launch";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 5);
+            this.listBox1.MultiColumn = true;
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(259, 173);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private CheckBox checkBox1;
+        private ListBox listBox1;
+    }
+}
+
---
+++ Twitchy/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Diagnostics;
+
+namespace Twitchy
+{
+    
+    public partial class Form1 : Form
+    {
+        public Regex RegStreamers = new Regex("\"name\":\"(.*?)\",");
+        public Regex RegGames = new Regex("\"game\":\"(.*?)\",");
+        public Regex RegTitles = new Regex("\"status\":\"(.*?)\",");
+
+        public String Streamer;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+        }
+
+        private void init()
+        {
+            List<String> ParsedStreamers = new List<string>();
+            List<String> ParsedGames = new List<string>();
+            List<String> ParsedTitles = new List<string>();
+            listBox1.Items.Clear();
+            listBox1.Items.Add("Loading...");
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.twitch.tv/kraken/streams/followed?oauth_token=nckhg93u1dfqqmoczk54vx5ly7w6cw");
+            WebResponse response = request.GetResponse();
+            string responseText;
+            using (Stream stream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+                responseText = reader.ReadToEnd();
+            }
+            foreach (Match a in RegStreamers.Matches(responseText.ToString()))
+            {
+                ParsedStreamers.Add(a.Groups[1].ToString());
+            }
+            foreach (Match a in RegGames.Matches(responseText.ToString()))
+            {
+                ParsedGames.Add(a.Groups[1].ToString());
+            }
+            foreach (Match a in RegTitles.Matches(responseText.ToString()))
+            {
+                ParsedTitles.Add(a.Groups[1].ToString());
+            }
+            //MessageBox.Show(ParsedGames.Count.ToString() + ParsedStreamers.Count.ToString() + ParsedTitles.Count.ToString());
+            listBox1.Items.Clear();
+            if (ParsedGames.Count / 2 == ParsedStreamers.Count && ParsedGames.Count / 2 == ParsedTitles.Count)
+            {
+                String[] a = ParsedStreamers.ToArray();
+                String[] b = ParsedGames.ToArray();
+                String[] c = ParsedTitles.ToArray();
+
+                for (int x = 0; x < ParsedStreamers.ToArray().Length; x++)
+                {
+                    string temp = a[x] + "," + b[x] + "," + c[x];
+                    listBox1.Items.Add(temp);
+                }
+            }
+            else
+            {
+                listBox1.Items.Add("Twitch API Response is malformed.");
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Streamer = listBox1.SelectedItem.ToString().Split(',')[0].ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            init();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process livestreamer = new Process();
+            livestreamer.StartInfo.FileName = "livestreamer\\livestreamer.exe";
+            if(Streamer != null){
+                livestreamer.StartInfo.Arguments = "-p MPC-HC\\mpc-hc.exe twitch.tv/" + Streamer + " best";
+                livestreamer.Start();
+                //MessageBox.Show(Streamer);
+            }
+        }
+    }
+}
