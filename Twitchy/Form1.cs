6235c20edea989cb69acaeb5aed5f6e89d7e8792
justastranger <justastrangerlica@gmail.com>
2015-05-14 22:40:16 -0500

    cleanup and static electricity

--- Twitchy/Form1.cs
+++ Twitchy/Form1.cs
@@ -17,17 +17,25 @@ namespace Twitchy
     
     public partial class Form1 : Form
     {
-        public FileStream oauth = File.Open(AppDomain.CurrentDomain.BaseDirectory + "\\oauth", FileMode.OpenOrCreate);
-        public string oauthToken;
-        public Regex RegStreamers = new Regex("\"name\":\"(.*?)\",");
-        public Regex RegGames = new Regex("\"game\":\"(.*?)\",");
-        public Regex RegTitles = new Regex("\"status\":\"(.*?)\",");
+        public static FileStream oauth = File.Open(AppDomain.CurrentDomain.BaseDirectory + "\\oauth", FileMode.OpenOrCreate);
+        public static string oauthToken;
+        public static Regex RegStreamers = new Regex("\"name\":\"(.*?)\",");
+        public static Regex RegGames = new Regex("\"game\":\"(.*?)\",");
+        public static Regex RegTitles = new Regex("\"status\":\"(.*?)\",");
 
         public String Streamer;
         
         public Form1()
-        {
+        {   // This is essentially Main() at this point.
             InitializeComponent();
+            checkOauth();
+            oauth.Flush();
+            oauth.Close();
+            init();
+        }
+
+        private static void checkOauth()
+        {
             if (new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "\\oauth").Length == 0)
             {
                 oauthToken = Twitchy.Prompt.ShowDialog("Please enter your Oauth key.", "No OAuth Saved");
@@ -35,27 +43,27 @@ namespace Twitchy
             }
             else
             {
-                using( StreamReader reader = new StreamReader(oauth)){
+                using (StreamReader reader = new StreamReader(oauth))
+                {
                     oauthToken = reader.ReadToEnd();
                 }
             }
-            oauth.Close();
-            init();
         }
 
         private static void AddText(FileStream fs, string value)
-        {
+        {   // Stole this from StackOverflow somewhere, google it for credits.
             byte[] info = new UTF8Encoding(true).GetBytes(value);
             fs.Write(info, 0, info.Length);
         }
 
         private void init()
         {
-            List<String> ParsedStreamers = new List<string>();
+            List<String> ParsedStreamers = new List<string>(); // Prepare lists to store names, games, and titles.
             List<String> ParsedGames = new List<string>();
             List<String> ParsedTitles = new List<string>();
             listBox1.Items.Clear();
             listBox1.Items.Add("Loading...");
+                                // Ask Twitch's API nicely to see who our user is following
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.twitch.tv/kraken/streams/followed?oauth_token="+oauthToken);
             WebResponse response = request.GetResponse();
             string responseText;
@@ -66,39 +74,38 @@ namespace Twitchy
             }
             foreach (Match a in RegStreamers.Matches(responseText.ToString()))
             {
-                ParsedStreamers.Add(a.Groups[1].ToString());
+                ParsedStreamers.Add(a.Groups[1].ToString()); // regex the response looking for streamer names
             }
             foreach (Match a in RegGames.Matches(responseText.ToString()))
             {
-                ParsedGames.Add(a.Groups[1].ToString());
+                ParsedGames.Add(a.Groups[1].ToString());     // Games
             }
             foreach (Match a in RegTitles.Matches(responseText.ToString()))
             {
-                ParsedTitles.Add(a.Groups[1].ToString());
+                ParsedTitles.Add(a.Groups[1].ToString());    // and lame-ass titles
             }
-            //MessageBox.Show(ParsedGames.Count.ToString() + ParsedStreamers.Count.ToString() + ParsedTitles.Count.ToString());
-            listBox1.Items.Clear();
+            listBox1.Items.Clear(); // Make sure that the listbox is empty before we start thinking of actually populating it
             if (ParsedGames.Count / 2 == ParsedStreamers.Count && ParsedGames.Count / 2 == ParsedTitles.Count)
             {
-                String[] a = ParsedStreamers.ToArray();
+                String[] a = ParsedStreamers.ToArray(); // Temp arrays for joining in a moment.
                 String[] b = ParsedGames.ToArray();
                 String[] c = ParsedTitles.ToArray();
 
-                for (int x = 0; x < ParsedStreamers.ToArray().Length; x++)
+                for (int x = 0; x < a.Length; x++)
                 {
-                    string temp = a[x] + "," + b[x] + "," + c[x];
-                    listBox1.Items.Add(temp);
+                    string temp = a[x] + "," + b[x] + "," + c[x]; // Will replace that soon with something allowing multiple columns
+                    listBox1.Items.Add(temp); // Populate the list
                 }
             }
             else
             {
-                listBox1.Items.Add("Twitch API Response is malformed.");
+                listBox1.Items.Add("Twitch API Response is malformed."); // The numbers don't match up.
             }
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            Streamer = listBox1.SelectedItem.ToString().Split(',')[0].ToString();
+            Streamer = listBox1.SelectedItem.ToString().Split(',')[0].ToString(); // Grabs the name of the streamer, since it's always the first thing before the first comma.
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -113,7 +120,6 @@ namespace Twitchy
             if(Streamer != null){
                 livestreamer.StartInfo.Arguments = "-p MPC-HC\\mpc-hc.exe twitch.tv/" + Streamer + " best";
                 livestreamer.Start();
-                //MessageBox.Show(Streamer);
             }
         }
     }
