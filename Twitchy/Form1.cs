87b7a84277f9a4b9195e1d64bc81edb81a713269
justastranger <justastrangerlica@gmail.com>
2015-05-17 17:17:38 -0500

    You'll now be asked for a new OAuth token if the one previously provided proves to be invalid.

--- Twitchy/Form1.cs
+++ Twitchy/Form1.cs
@@ -21,6 +21,7 @@ namespace Twitchy
         // FileStream will fail and the app will crash if we don't have permissions
         public static FileStream oauth = File.Open(AppDomain.CurrentDomain.BaseDirectory + "\\oauth", FileMode.OpenOrCreate);
         public static string oauthToken;
+        public static bool valid = true;
 
         public String Streamer;
         
@@ -28,13 +29,19 @@ namespace Twitchy
         {   // This is essentially Main() at this point.
             // Split everything into smaller tasks
             InitializeComponent();
-            checkOauth();
             init();
             dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
         }
 
         private static void checkOauth()
         {
+            if (!valid)
+            {
+                oauth.Close();
+                oauth = File.Open(AppDomain.CurrentDomain.BaseDirectory + "\\oauth", FileMode.Create);
+                oauthToken = null;
+            }
+
             if (oauthToken == null)
             {
                 if (new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "\\oauth").Length == 0)
@@ -70,8 +77,9 @@ namespace Twitchy
         }
 
 
-        private void init()
+        private void init() 
         {
+            checkOauth();
             Regex RegStreamers = new Regex("\"name\":\"(.*?)\",");
             Regex RegGames = new Regex("\"game\":\"(.*?)\",");
             Regex RegTitles = new Regex("\"status\":\"(.*?)\",");
@@ -83,55 +91,70 @@ namespace Twitchy
             dataGridView1.Rows.Clear();
                                 // Ask Twitch's API nicely if we can see who our user is following
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.twitch.tv/kraken/streams/followed?oauth_token="+oauthToken);
-            WebResponse response = request.GetResponse();
-            string responseText;
-            using (Stream stream = response.GetResponseStream())
-            {
-                StreamReader reader = new StreamReader(stream, Encoding.UTF8);
-                responseText = reader.ReadToEnd();
-            }
-            foreach (Match a in RegStreamers.Matches(responseText.ToString()))
-            {
-                ParsedStreamers.Add(a.Groups[1].ToString()); // regex the response looking for streamer names
-            }
-            foreach (Match a in RegGames.Matches(responseText.ToString()))
-            {
-                ParsedGames.Add(a.Groups[1].ToString());     // Games
-            }
-            foreach (Match a in RegTitles.Matches(responseText.ToString()))
-            {
-                ParsedTitles.Add(a.Groups[1].ToString());    // and lame-ass titles
-            }
-            ParsedTitles = unescape(ParsedTitles);
-            // TODO add the ChrW nonsense for unescaping unicode characters
-            if (ParsedGames.Count / 2 == ParsedStreamers.Count && ParsedGames.Count / 2 == ParsedTitles.Count)
-            {
-                using(var ps = ParsedStreamers.GetEnumerator())
-                using(var pg = ParsedGames.GetEnumerator())
-                using(var pt = ParsedTitles.GetEnumerator()){
-                    while (ps.MoveNext() && pg.MoveNext() && pt.MoveNext())
+            
+            try {
+                string responseText;
+                WebResponse response = request.GetResponse();
+                using (Stream stream = response.GetResponseStream())
+                {   // Convert Twitch's response into a string.
+                   StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+                   responseText = reader.ReadToEnd();
+                }
+                foreach (Match a in RegStreamers.Matches(responseText.ToString()))
+                {
+                    ParsedStreamers.Add(a.Groups[1].ToString()); // regex the response looking for and populating the lists with streamer names
+                }
+                foreach (Match a in RegGames.Matches(responseText.ToString()))
+                {
+                    ParsedGames.Add(a.Groups[1].ToString());     // Games
+                }
+                foreach (Match a in RegTitles.Matches(responseText.ToString()))
+                {
+                    ParsedTitles.Add(a.Groups[1].ToString());    // and lame-ass titles
+                }
+                ParsedTitles = unescape(ParsedTitles);  // Unescaoe the titles so you can see the fancy unicode stuff people come up with.
+                if (ParsedGames.Count / 2 == ParsedStreamers.Count && ParsedGames.Count / 2 == ParsedTitles.Count)
+                {
+                    using (var ps = ParsedStreamers.GetEnumerator())
+                    using (var pg = ParsedGames.GetEnumerator())
+                    using (var pt = ParsedTitles.GetEnumerator())
                     {
-                        pg.MoveNext();
-                        //string temp = ps.Current + "," + pg.Current + "," + pt.Current; // Will replace that soon with something allowing multiple columns
-                        //listBox1.Items.Add(temp); // Populate the list
-                        using (DataGridViewRow row = new DataGridViewRow())
-                        {
-                            //row.SetValues(new string[] { ps.Current, pg.Current, pt.Current });
-                            row.CreateCells(dataGridView1, new string[] { ps.Current, pg.Current, pt.Current });
-                            dataGridView1.Rows.Add(row);
+                        while (ps.MoveNext() && pg.MoveNext() && pt.MoveNext())
+                        {   // Skip ever other game name, since I can't be arsed to deduplicate.
+                            pg.MoveNext();
+                            using (DataGridViewRow row = new DataGridViewRow())
+                            {   // Populate the DataGridView.
+                                row.CreateCells(dataGridView1, new string[] { ps.Current, pg.Current, pt.Current });
+                                dataGridView1.Rows.Add(row);
+                            }
                         }
                     }
                 }
-            }
-            else
-            {
-                using (DataGridViewRow a = new DataGridViewRow())
+                else
                 {
-                    a.CreateCells(dataGridView1, "Twitch API Response is malformed.");
-                    dataGridView1.Rows.Add(a);
+                    using (DataGridViewRow a = new DataGridViewRow())
+                    {   // Complain about Twitch's api response not being constant
+                        a.CreateCells(dataGridView1, "Twitch API Response is malformed.");
+                        dataGridView1.Rows.Add(a);
+                    }
                 }
-                //listBox1.Items.Add("Twitch API Response is malformed."); // The numbers don't match up.
+                valid = true;
             }
+            catch (WebException e)
+            {
+                if (e.Message.Contains("401"))
+                {
+                    valid = false;
+                    using (DataGridViewRow a = new DataGridViewRow())
+                    {   // Complain about the invalid OAuth token.
+                        a.CreateCells(dataGridView1, new string[] { "Invalid OAuth Token.", "Please press refresh."});
+                        dataGridView1.Rows.Add(a);
+                    }
+                }
+            }
+            
+            
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -142,24 +165,23 @@ namespace Twitchy
         private void button1_Click(object sender, EventArgs e)
         {   // Start Stream
             if (Streamer == null)
-            {
+            {   // If no streamer is selected, complain and break.
                 MessageBox.Show("Select a streamer.");
+                return;
             }
             Process livestreamer = new Process();
-            livestreamer.StartInfo.FileName = ".\\livestreamer\\livestreamer.exe";
-            if(Streamer != null){
-                if (checkBox2.Checked)
-                {
-                    //http://www.twitch.tv/{Streamer}/chat
-                    Form chat = new Form2("http://www.twitch.tv/" + Streamer + "/chat");
-                    chat.Closed += new EventHandler(context.OnFormClosed);
-                    chat.FormClosed += new FormClosedEventHandler(context.OnFormClosed);
-                    chat.Show();
-                }
-                livestreamer.StartInfo.Arguments = "-p MPC-HC\\mpc-hc.exe twitch.tv/" + Streamer + " best";
-                livestreamer.Start();
-                if (checkBox1.Checked) this.Close();
+            livestreamer.StartInfo.FileName = @".\livestreamer\livestreamer.exe";
+            if (checkBox2.Checked)
+            {
+                //http://www.twitch.tv/{Streamer}/chat
+                Form chat = new Form2("http://www.twitch.tv/" + Streamer + "/chat");
+                chat.Closed += new EventHandler(context.OnFormClosed); // Add event handlers so that everything closes tidily
+                chat.FormClosed += new FormClosedEventHandler(context.OnFormClosed);
+                chat.Show();
             }
+            livestreamer.StartInfo.Arguments = @"-p MPC-HC\mpc-hc.exe twitch.tv/" + Streamer + " best";
+            livestreamer.Start();
+            if (checkBox1.Checked) this.Close();
         }
 
         private void dataGridView1_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
@@ -168,18 +190,17 @@ namespace Twitchy
         }
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            //if (dataGridView1.CurrentCell != null)
+        {   // Get the contents of the first sell in the selected row.
             Streamer = (dataGridView1.CurrentRow.Cells[0].Value.ToString());
         }
 
         private void Form1_Resize(object sender, EventArgs e)
-        {
+        {   // Dirty hack to fit the table's elements.
             dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
         }
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
-        {
+        {   // Event handler to make sure everything closes tidily.
             context.OnFormClosed(sender, e);
         }
     }
