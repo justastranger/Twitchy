ed94af9be9b97a3f3eff748895de61c6fe5f51dc
Michael Richards <justastranger@users.noreply.github.com>
2018-04-06 16:01:24 -0500

    Update ChatWindow.cs
    Twitch just deprecated twitch.tv/<channel>/chat in favor of twitch.tv/popout/<channel>/chat.

--- Twitchy/ChatWindow.cs
+++ Twitchy/ChatWindow.cs
@@ -13,7 +13,7 @@ namespace Twitchy
         
         public static void ShowChat(string streamer){
             //http://www.twitch.tv/{Streamer}/chat
-            Form chat = new ChatWindow("http://www.twitch.tv/" + streamer + "/chat");
+            Form chat = new ChatWindow("http://www.twitch.tv/popout/" + streamer + "/chat");
             chat.Closed += new EventHandler(context.OnFormClosed); // Add event handlers so that everything closes tidily
             chat.FormClosed += new FormClosedEventHandler(context.OnFormClosed);
             chat.Show();
